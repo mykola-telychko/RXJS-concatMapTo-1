8a62adefd15ddc4505964a2804600c99423a82ff
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-31 01:04:58 +0300

    fin

--- index.ts
+++ index.ts
@@ -6,14 +6,13 @@ import { of } from 'rxjs/observable/of';
 import { interval } from 'rxjs/observable/interval';
 import { concatMapTo, delay, take } from 'rxjs/operators';
 
-
 // https://www.learnrxjs.io/learn-rxjs/operators/transformation/concatmapto
 // Example 1: Map to basic observable (simulating request)
 
 //emit value every 2 seconds
-const sampleInterval = interval(500).pipe(take(5));
+const srcInterval$ = interval(500).pipe(take(5));
 const fakeRequest = of('Network request complete').pipe(delay(3000));
 //wait for first to complete before next is subscribed
-const example = sampleInterval.pipe(concatMapTo(fakeRequest));
+const example = srcInterval$.pipe(concatMapTo(fakeRequest));
 //output: Network request complete...3s...Network request complete'
 const subscribe = example.subscribe((val) => console.log(val));
